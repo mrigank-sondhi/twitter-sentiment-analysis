3b2b45c7424d0f56a314683199a85a23491306ea
mrigank-sondhi <you@example.com>
2022-06-28 10:59:46 +0530

    add dynamic and static folders

---
+++ Dynamic/settings.py
@@ -0,0 +1,6 @@
+TRACK_WORDS = "facebook"
+TABLE_NAME = "Facebook"
+TABLE_ATTRIBUTES = "tweet_id VARCHAR(255), created_at DATETIME, text VARCHAR(1000), \
+            polarity INT, subjectivity INT, user_created_at DATETIME, user_location VARCHAR(255), \
+            user_description VARCHAR(255), user_followers_count INT, longitude DOUBLE, latitude DOUBLE, \
+            retweet_count INT, like_count INT"
